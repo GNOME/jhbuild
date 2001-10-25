d52eb58d4565b6470c787bba924cb1e7104e8971
James Henstridge <jamesh@src.gnome.org>
2001-10-25 01:28:06 +0000

    updates to module information

--- moduleinfo.py
+++ moduleinfo.py
@@ -1,7 +1,7 @@
 from module import Module, ModuleSet
 
 head = ModuleSet()
-head.add(Module(name='xml-i18n-tools'))
+head.add(Module(name='intltool'))
 head.add(Module(name='gnome-common'))
 head.add(Module(name='gtk-doc'))
 head.add(Module(name='glib',
@@ -15,22 +15,22 @@ head.add(Module(name='linc', dependencies=['glib']))
 head.add(Module(name='libIDL', dependencies=['glib']))
 head.add(Module(name='ORBit2', dependencies=['linc', 'libIDL']))
 head.add(Module(name='bonobo-activation',
-         dependencies=['xml-i18n-tools', 'gnome-common','ORBit2','gnome-xml']))
+         dependencies=['intltool', 'gnome-common','ORBit2','gnome-xml']))
 head.add(Module(name='gconf', dependencies=['ORBit2','gnome-xml','gtk+']))
 head.add(Module(name='libbonobo', dependencies=['ORBit2','bonobo-activation']))
 head.add(Module(name='gnome-vfs', dependencies=['libbonobo','gconf']))
 head.add(Module(name='libart_lgpl'))
 head.add(Module(name='bonobo-config',
-                dependencies=['libbonobo', 'xml-i18n-tools']))
+                dependencies=['libbonobo', 'intltool']))
 head.add(Module(name='libgnome',
                 dependencies=['libbonobo','gnome-vfs','gconf']))
-head.add(Module(name='libgnomecanvas', dependencies=['gtk+', 'libart_lgpl']))
+head.add(Module(name='libgnomecanvas', dependencies=['gtk+', 'libart_lgpl', 'libglade']))
 head.add(Module(name='libbonoboui',
-                dependencies=['libgnome', 'libbonobo', 'libgnomecanvas']))
-head.add(Module(name='libgnomeui', dependencies=['libbonoboui']))
+                dependencies=['libgnome', 'libbonobo', 'libgnomecanvas', 'libglade']))
+head.add(Module(name='libgnomeui', dependencies=['libbonoboui', 'libglade']))
 
 head.add(Module(name='libglade',
-         dependencies=['gtk+', 'gnome-xml', 'libbonoboui', 'libgnomeui']))
+         dependencies=['gtk+', 'gnome-xml']))
 head.add(Module(name='gnome-python/pygtk',
          dependencies=['gtk+', 'libglade']))
 head.add(Module(name='gnome-python/gnome-python',
