1e3e98c3d31fc05d98e4d4884e634fce8c08e66c
Ian McKellar <yakk@src.gnome.org>
2003-06-25 04:54:38 +0000

    Fixed error reporting
            * cvs.py:
            Fixed error reporting

--- cvs.py
+++ cvs.py
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import os, string
+import os, string, sys
 
 # table used to scramble passwords in ~/.cvspass files
 _shifts = [
@@ -79,7 +79,7 @@ def login(cvsroot, password=None):
     else:
         # call cvs login ..
         if os.system('cvs -d %s login' % cvsroot) != 0:
-            sys.stderr('could not log into %s\n' % cvsroot)
+            sys.stderr.write('could not log into %s\n' % cvsroot)
             sys.exit(1)
 
 class CVSRoot:
