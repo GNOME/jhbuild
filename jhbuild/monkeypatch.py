ef53449cc2f04d38ed269787a7af6c46b56b3729
James Henstridge <james@jamesh.id.au>
2005-06-21 08:10:21 +0000

    a module to add features to the standard library that don't exist in old
    2005-06-21  James Henstridge  <james@jamesh.id.au>
    
    	* jhbuild/monkeypatch.py: a module to add features to the standard
    	library that don't exist in old versions.  At the moment, it just
    	includes the True/False fix.
    
    	* jhbuild/__init__.py: move monkeypatch code out into a separate
    	file.

--- jhbuild/monkeypatch.py
+++ jhbuild/monkeypatch.py
@@ -1,5 +1,5 @@
 # jhbuild - a build script for GNOME 1.x and 2.x
-# Copyright (C) 2001-2004  James Henstridge
+# Copyright (C) 2001-2005  James Henstridge
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,5 +15,19 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from jhbuild import monkeypatch
-del monkeypatch
+# extras not found in old versions of Python
+
+# add True and False constants, for the benefit of Python < 2.2.1
+import __builtin__
+if not hasattr(__builtin__, 'True'):
+    __builtin__.True = (1 == 1)
+    __builtin__.False = (1 != 1)
+
+## if not hasattr(__builtin__, 'enumerate'):
+##     def enumerate(iterable):
+##         index = 0
+##         for item in iterable:
+##             yield (index, item)
+##             index += 1
+##     __builtin__.enumerate = enumerate
+
