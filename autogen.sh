b4a44835bac54ec48edc381ce3feefe7c7037645
Frederic Peters <fpeters@src.gnome.org>
2007-08-13 20:51:20 +0000

    added more notes about autotools being used just for the documentation.
    * autogen.sh, configure.ac: added more notes about autotools being used
    just for the documentation.
    
    
    svn path=/trunk/; revision=1570

--- autogen.sh
+++ autogen.sh
@@ -13,9 +13,6 @@ PKG_NAME=jhbuild
 }
 
 which gnome-autogen.sh || {
-    echo "Autotools are only used to build documentation"
-    echo "Type make -f Makefile.plain to build or install JhBuild"
-    echo ""
     echo "If you want to build documentation, you need to install gnome-common"
     echo "from the GNOME Subversion."
     exit 1
@@ -26,5 +23,14 @@ REQUIRED_AUTOCONF_VERSION=2.57
 REQUIRED_AUTOMAKE_VERSION=1.8
 REQUIRED_INTLTOOL_VERSION=0.35.0
 REQUIRED_PKG_CONFIG_VERSION=0.16.0
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes gnome-autogen.sh
+if test "$?" != "0"
+then
+	cat << _EOF_
+
+Note that autotools are only used to build documentation
+Type make -f Makefile.plain to build or install JHBuild
+
+_EOF_
+fi
 
