f7588409976639805ab975e1bde945db5d82350a
Frederic Peters <fpeters@src.gnome.org>
2007-08-25 16:16:35 +0000

    disable doc build/install by default, so make install works exactly like
    * INSTALL, autogen.sh, configure.ac, doc/Makefile.am: disable doc
    build/install by default, so make install works exactly like before,
    installing stuff in ~, not trying to install help files in $prefix.
    
    
    svn path=/trunk/; revision=1579

--- autogen.sh
+++ autogen.sh
@@ -23,5 +23,14 @@ REQUIRED_AUTOCONF_VERSION=2.57
 REQUIRED_AUTOMAKE_VERSION=1.8
 REQUIRED_INTLTOOL_VERSION=0.35.0
 REQUIRED_PKG_CONFIG_VERSION=0.16.0
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes gnome-autogen.sh $@
+if test "$?" != "0"
+then
+        cat << _EOF_
+
+Note that autotools are only required to build documentation;
+type make -f Makefile.plain to build or install JHBuild without
+the documentation
+_EOF_
+fi
 
