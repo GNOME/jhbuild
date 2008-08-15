da71c14f2728b771634865e68c81e65489635d1f
Frederic Peters <fpeters@src.gnome.org>
2008-08-15 09:47:16 +0000

    let master honor the skip configuration variable.
    * jhbuild/buildbot/__init__.py: let master honor the skip configuration
    variable.
    
    
    svn path=/trunk/; revision=2241

--- jhbuild/buildbot/__init__.py
+++ jhbuild/buildbot/__init__.py
@@ -20,8 +20,9 @@
 import jhbuild.moduleset
 
 def jhbuild_list():
-    config = jhbuild.config.Config()
     module_set = jhbuild.moduleset.load(jhbuild_config)
-    module_list = module_set.get_module_list(config.modules,
+    module_list = module_set.get_module_list(
+            jhbuild_config.modules,
+            jhbuild_config.skip,
             include_optional_modules=True)
     return [x.name for x in module_list if not x.name.startswith('meta-')]
