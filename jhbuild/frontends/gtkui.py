4b71b6d56661b48ce079bb87fe622640a951aa7d
Frédéric Péters <fpeters@0d.be>
2009-05-17 12:58:17 +0200

    [gui] don't advance progress bar for first element

--- jhbuild/frontends/gtkui.py
+++ jhbuild/frontends/gtkui.py
@@ -211,13 +211,14 @@ class AppWindow(gtk.Window, buildscript.BuildScript):
         self.module_hbox.set_sensitive(False)
 
     def end_build(self, failures):
+        self.progressbar.set_fraction(1)
         self.progressbar.set_text(_('Build Completed'))
         self.build_button.set_sensitive(True)
         self.module_hbox.set_sensitive(True)
 
     def start_module(self, module):
         idx = [x.name for x in self.modulelist].index(module)
-        self.progressbar.set_fraction((1.0+idx) / len(self.modulelist))
+        self.progressbar.set_fraction((1.0*idx) / len(self.modulelist))
         if vte:
             self.terminal.feed('%s*** %s ***%s\n\r' % (t_bold, module, t_reset))
 
