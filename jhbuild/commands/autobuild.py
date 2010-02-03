GNOME/jhbuild

commit ee4bf954709024d04527eff4b9c33ea0ee4b3745

[commands] simplify command line help (GNOME bug 606360)
This removes the distinction between --help and --help-commands, and adds a
"help" command (equivalent to --help).

jhbuild/commands/base.py
@@ -376,7 +376,7 @@ class cmd_run(Command):
                         help=_('run command in checkout dir of the given module')),
             ])
 
-    def execute(self, config, args):
+    def execute(self, config, args, help=None):
         # Do a shallow check of the arguments list
         # so that '--' isn't always required when command has arguments, 
         # only if some of them look like they might be for us
@@ -442,7 +442,7 @@ class cmd_shell(Command):
     name = 'shell'
     usage_args = ''
 
-    def execute(self, config, args):
+    def execute(self, config, args, help=None):
         if "--help" in args:
             self.parse_args(args) # This doesn't return
         user_shell = os.environ.get('SHELL', '/bin/sh')

jhbuild/commands/__init__.py
@@ -47,9 +47,9 @@ class Command:
     def __init__(self, options=[]):
         self.options = options
 
-    def execute(self, config, args):
+    def execute(self, config, args, help):
         options, args = self.parse_args(args)
-        return self.run(config, options, args)
+        return self.run(config, options, args, help)
 
     def parse_args(self, args):
         self.parser = OptionParser(
@@ -58,20 +58,56 @@ class Command:
         self.parser.add_options(self.options)
         return self.parser.parse_args(args)
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         """The body of the command"""
         raise NotImplementedError
 
+
+def print_help():
+    import os
+    thisdir = os.path.abspath(os.path.dirname(__file__))
+
+    # import all available commands
+    for fname in os.listdir(os.path.join(thisdir)):
+        name, ext = os.path.splitext(fname)
+        if not ext == '.py':
+            continue
+        try:
+            __import__('jhbuild.commands.%s' % name)
+        except ImportError:
+            pass
+
+    uprint(_('JHBuild commands are:'))
+    commands = [(x.name, x.doc) for x in get_commands().values()]
+    commands.sort()
+    for name, description in commands:
+        uprint('  %-15s %s' % (name, description))
+    print
+    uprint(_('For more information run "jhbuild <command> --help"'))
 
 # handle registration of new commands
 _commands = {}
 def register_command(command_class):
     _commands[command_class.name] = command_class
 
+# special help command, never run
+class cmd_help(Command):
+    doc = N_('Information about available jhbuild commands')
+
+    name = 'help'
+    usage_args = ''
+
+    def run(self, config, options, args, help=None):
+        if help:
+            return help()
+
+register_command(cmd_help)
+
+
 def get_commands():
     return _commands
 
-def run(command, config, args):
+def run(command, config, args, help):
     # if the command hasn't been registered, load a module by the same name
     if command not in _commands:
         try:
@@ -82,8 +118,9 @@ def run(command, config, args):
         raise FatalError(_('command not found'))
 
     command_class = _commands[command]
+
     cmd = command_class()
-    return cmd.execute(config, args)
+    return cmd.execute(config, args, help)
 
 
 from jhbuild.commands import base

jhbuild/main.py
@@ -75,26 +75,10 @@ class LoggingFormatter(logging.Formatter):
         record.level_name_initial = record.levelname[0]
         return logging.Formatter.format(self, record)
 
-def help_commands(option, opt_str, value, parser):
-    thisdir = os.path.abspath(os.path.dirname(__file__))
-    
-    # import all available commands
-    for fname in os.listdir(os.path.join(thisdir, 'commands')):
-        name, ext = os.path.splitext(fname)
-        if not ext == '.py':
-            continue
-        try:
-            __import__('jhbuild.commands.%s' % name)
-        except ImportError:
-            pass
-    
-    uprint(_('JHBuild commands are:'))
-    commands = [(x.name, x.doc) for x in jhbuild.commands.get_commands().values()]
-    commands.sort()
-    for name, description in commands:
-        uprint('  %-15s %s' % (name, description))
+def print_help(parser):
+    parser.print_help()
     print
-    uprint(_('For more information run "jhbuild <command> --help"'))
+    jhbuild.commands.print_help()
     parser.exit()
 
 def main(args):
@@ -109,11 +93,15 @@ def main(args):
     logging.getLogger().addHandler(logging_handler)
     parser = optparse.OptionParser(
         usage=_('%prog [ -f config ] command [ options ... ]'),
+        add_help_option=False,
         description=_('Build a set of modules from diverse repositories in correct dependency order (such as GNOME).'))
     parser.disable_interspersed_args()
+    parser.add_option('-h', '--help', action='callback',
+                      callback=lambda *args: print_help(parser),
+                      help=_("Display this help and exit"))
     parser.add_option('--help-commands', action='callback',
-                      callback=help_commands,
-                      help=_('Information about available jhbuild commands'))
+                      callback=lambda *args: print_help(parser),
+                      help=optparse.SUPPRESS_HELP)
     parser.add_option('-f', '--file', action='store', metavar='CONFIG',
                       type='string', dest='configfile',
                       default=os.environ.get("JHBUILDRC", os.path.join(os.environ['HOME'], '.jhbuildrc')),
@@ -145,7 +133,7 @@ def main(args):
     warn_local_modulesets(config)
 
     try:
-        rc = jhbuild.commands.run(command, config, args)
+        rc = jhbuild.commands.run(command, config, args, help=lambda: print_help(parser))
     except UsageError, exc:
         sys.stderr.write('jhbuild %s: %s\n' % (command, exc.args[0].encode(_encoding, 'replace')))
         parser.print_usage()

commit 49e02db2448d341103cf14387a3e7c259b806531

Fixed calls to Command.run(), broken by ee4bf954709024d0 (GNOME bug 608875)

jhbuild/commands/autobuild.py
@@ -49,8 +49,8 @@ class cmd_autobuild(Command):
                         action='store_true', dest='verbose', default=False,
                         help=_('verbose mode')),
             ])
-    
-    def run(self, config, options, args):
+
+    def run(self, config, options, args, help=None):
         config.buildscript = 'autobuild'
 
         config.autobuild_report_url = None

jhbuild/commands/base.py
@@ -58,7 +58,7 @@ class cmd_update(Command):
                         help=_('ignore all soft-dependencies')),
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         config.set_from_cmdline_options(options)
         module_set = jhbuild.moduleset.load(config)
         module_list = module_set.get_module_list(args or config.modules,
@@ -94,7 +94,7 @@ class cmd_updateone(Command):
                         help=_('set a sticky date when checking out modules')),
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         config.set_from_cmdline_options(options)
         module_set = jhbuild.moduleset.load(config)
         try:
@@ -128,7 +128,7 @@ class cmd_cleanone(Command):
                         help=_('honour the makeclean setting in config file')),
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         if options.honour_config is False:
             config.makeclean = True
         module_set = jhbuild.moduleset.load(config)
@@ -266,7 +266,7 @@ class cmd_build(Command):
                         help=_('skip modules installed less than the given time ago')),
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         config.set_from_cmdline_options(options)
 
         if not config.quiet_mode:
@@ -339,7 +339,7 @@ class cmd_buildone(Command):
                         help=_('skip modules installed less than the given time ago')),
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         config.set_from_cmdline_options(options)
 
         if not config.quiet_mode:
@@ -389,7 +389,7 @@ class cmd_run(Command):
             raise FatalError(_("Unable to execute the command '%(command)s': %(err)s") % {
                     'command':args[0], 'err':str(exc)})
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         if options.in_builddir:
             module_set = jhbuild.moduleset.load(config)
             try:
@@ -482,7 +482,7 @@ class cmd_list(Command):
                         help=_('list all modules, not only those that would be built')),
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         config.set_from_cmdline_options(options)
         module_set = jhbuild.moduleset.load(config)
         if options.list_all_modules:
@@ -529,7 +529,7 @@ class cmd_dot(Command):
                         help=_('group modules from metamodule together')),
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         module_set = jhbuild.moduleset.load(config)
         if args:
             modules = args

jhbuild/commands/bootstrap.py
@@ -42,7 +42,7 @@ class cmd_bootstrap(cmd_build):
                         action='store_true', dest='ignore_system', default=False,
                         help=_('do not use system installed modules')))
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         config.moduleset = 'bootstrap'
         # load the bootstrap module set
         if not args:

jhbuild/commands/bot.py
@@ -99,7 +99,7 @@ class cmd_bot(Command):
                         help=_('exec a buildbot step (internal use only)')),
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         if options.setup:
             return self.setup(config)
 

jhbuild/commands/checkbranches.py
@@ -33,7 +33,7 @@ class cmd_checkbranches(Command):
             make_option('-b', '--branch', metavar = 'BRANCH',
                     action = 'store', dest = 'branch', default = None)])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         if options.branch:
             branch = options.branch
         else:

jhbuild/commands/checkmodulesets.py
@@ -30,7 +30,7 @@ class cmd_checkmodulesets(Command):
     doc = N_('Check if modules in jhbuild have the correct definition')
     name = 'checkmodulesets'
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         module_set = jhbuild.moduleset.load(config)
         module_list = module_set.get_full_module_list()
         for mod in module_list:

jhbuild/commands/clean.py
@@ -41,7 +41,7 @@ class cmd_clean(Command):
                         help=_('start building at the given module')),
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         for item in options.skip:
             config.skip += item.split(',')
 

jhbuild/commands/gui.py
@@ -32,7 +32,7 @@ class cmd_gui(Command):
     name = 'gui'
     usage_args = ''
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         # request GTK build script.
         config.buildscript = 'gtkui'
 

jhbuild/commands/info.py
@@ -40,7 +40,7 @@ class cmd_info(Command):
     usage_args = N_('[ modules ... ]')
 
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         packagedb = jhbuild.frontends.get_buildscript(config, []).packagedb
         module_set = jhbuild.moduleset.load(config)
 

jhbuild/commands/rdepends.py
@@ -40,7 +40,7 @@ class cmd_rdepends(Command):
                         help=_('limit display to modules directly depending on given module'))
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         module_set = jhbuild.moduleset.load(config)
 
         if not args:

jhbuild/commands/sanitycheck.py
@@ -54,7 +54,7 @@ class cmd_sanitycheck(Command):
     name = 'sanitycheck'
     usage_args = ''
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         if args:
             raise UsageError(_('no extra arguments expected'))
     

jhbuild/commands/snapshot.py
@@ -37,7 +37,7 @@ class cmd_snapshot(Command):
     def __init__(self):
         Command.__init__(self)
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         module_set = jhbuild.moduleset.load(config)
         module_list = module_set.get_module_list(args or config.modules,
                                                  config.skip)

jhbuild/commands/tinderbox.py
@@ -64,7 +64,7 @@ class cmd_tinderbox(Command):
                         help=_('build even if policy says not to'))
             ])
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         config.set_from_cmdline_options(options)
         config.buildscript = 'tinderbox'
 

jhbuild/commands/uninstall.py
@@ -31,7 +31,7 @@ class cmd_uninstall(Command):
     name = 'uninstall'
     usage_args = N_('[ modules ... ]')
 
-    def run(self, config, options, args):
+    def run(self, config, options, args, help=None):
         config.set_from_cmdline_options(options)
 
         module_set = jhbuild.moduleset.load(config)