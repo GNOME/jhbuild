0a7f5b799bb2a0bd6f5573e9b99e8ae9bbb4a8b8
Dirk Wallenstein <halsmit@t-online.de>
2010-06-05 13:28:01 +0200

    [info] Display git specific branch and tag information (GNOME bug 619935)

--- jhbuild/commands/info.py
+++ jhbuild/commands/info.py
@@ -83,6 +83,12 @@ class cmd_info(Command):
             uprint(_('Darcs-Archive:'), module.branch.module)
         elif isinstance(module.branch, GitBranch):
             uprint(_('Git-Module:'), module.branch.module)
+            git_branch = module.branch.branch
+            if not git_branch:
+                git_branch = 'master'
+            uprint(_('Git-Branch:'), git_branch)
+            if module.branch.tag:
+                uprint(_('Git-Tag:'), module.branch.tag)
         elif isinstance(module.branch, TarballBranch):
             uprint(_('URL:'), module.branch.module)
             uprint(_('Version:'), module.branch.version)
