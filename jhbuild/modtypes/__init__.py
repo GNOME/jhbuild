5f346cbcec01a10d7dbc046b0fae3bc0ab392929
Craig Keogh <cskeogh@adam.com.au>
2011-07-15 21:38:14 +0930

    Don't Fatal Error if module doesn't support DESTDIR (GNOME Bug 654592)

--- jhbuild/modtypes/__init__.py
+++ jhbuild/modtypes/__init__.py
@@ -263,7 +263,7 @@ them into the prefix."""
             os.rmdir(destdir)
 
         if not install_succeeded:
-            raise CommandError(_("Module failed to install into DESTDIR %(dest)r") % {'dest': broken_name})
+            logging.warn(_("Module failed to install into DESTDIR %(dest)r") % {'dest': broken_name})
 
     def get_revision(self):
         return self.branch.tree_id()
