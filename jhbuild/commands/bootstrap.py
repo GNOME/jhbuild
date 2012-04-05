092cd1feaee112dba11dd3b3b1a27ec3a0f06cb7
Craig Keogh <cskeogh@adam.com.au>
2012-04-05 14:16:19 +0930

    Make bootstrap build modules unconditionally (GNOME bug 656818)
    Revert of c896c9340755

--- jhbuild/commands/bootstrap.py
+++ jhbuild/commands/bootstrap.py
@@ -17,30 +17,13 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import os
-import sys
-import urllib
-from optparse import make_option
-import logging
-
-import jhbuild.moduleset
-import jhbuild.frontends
-from jhbuild.commands import Command, register_command
-import jhbuild.commands.base
+from jhbuild.commands import register_command
 from jhbuild.commands.base import cmd_build
-from jhbuild.utils.cmds import check_version
 
 class cmd_bootstrap(cmd_build):
     doc = N_('Build support tools')
 
     name = 'bootstrap'
-
-    def __init__(self):
-        cmd_build.__init__(self)
-        self.options.append(
-            make_option('--ignore-system',
-                        action='store_true', dest='ignore_system', default=False,
-                        help=_('do not use system installed modules')))
 
     def run(self, config, options, args, help=None):
         config.moduleset = 'bootstrap'
@@ -52,38 +35,7 @@ class cmd_bootstrap(cmd_build):
             config.skip += item.split(',')
         options.skip = []
 
-        ignored_modules = []
-        if not options.ignore_system:
-            # restore system PATH to check for system-installed programs
-            path = os.environ.get('PATH')
-            os.environ['PATH'] = path.replace(
-                    os.path.join(config.prefix, 'bin'), '')
-
-            module_set = jhbuild.moduleset.load(config)
-            modules = args or config.modules
-            module_list = module_set.get_module_list(modules)
-
-            for module in module_list:
-                if module.type == 'meta':
-                    continue
-                for version_regex in (r'.*?[ \(]([\d.]+)', r'^([\d.]+)'):
-                    if check_version([module.name, '--version'],
-                            version_regex, module.branch.version):
-                        ignored_modules.append(module.name)
-                        break
-
-            os.environ['PATH'] = path
-            config.skip.extend(ignored_modules)
-
         rc = cmd_build.run(self, config, options, args)
-
-        if ignored_modules:
-            logging.info(
-                    _('some modules (%s) were automatically ignored as a '
-                      'sufficient enough version was found installed on '
-                      'your system. Use --ignore-system if you want to build '
-                      'them nevertheless.' % ', '.join(ignored_modules)))
-
         return rc
 
 register_command(cmd_bootstrap)
