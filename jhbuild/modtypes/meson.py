GNOME/jhbuild

commit 73b433d89cd4c2496e9d44659a486d4ea0e2a20c

meson: fix clean/build/install if configure is skipped
This makes 'jhbuild buildone' work again. We need to always check for
the ninja binary.

jhbuild/modtypes/meson.py
@@ -52,6 +52,7 @@ class MesonModule(MakeModule, DownloadableModule):
         self.skip_install_phase = skip_install_phase
         self.force_non_srcdir_builds = True
         self.supports_install_destdir = True
+        self.find_ninja()
 
     def find_ninja(self):
         for f in ['ninja', 'ninja-build']:
@@ -100,7 +101,6 @@ class MesonModule(MakeModule, DownloadableModule):
         prefix = os.path.expanduser(buildscript.config.prefix)
         if not inpath('meson', os.environ['PATH'].split(os.pathsep)):
             raise CommandError(_('%s not found') % 'meson')
-        self.find_ninja()
         baseargs = '--prefix %s --libdir %s' % (prefix, self.get_libdir())
         mesonargs = self.get_mesonargs()
         cmd = 'meson %s %s %s' % (baseargs, mesonargs, srcdir)

commit 159367256a1cffa6f9daad0d10fa90f5bbff67ec

Fix 'jhbuild sysdeps' when ninja is not installed
This regressed in 73b433d89cd4c2496e9d44659a486d4ea0e2a20c, which is sad
because it means nobody has bothered to report that our Build GNOME
guide has been broken for the past two months.

We cannot check for ninja binary in __init__ because it gets called from
sysdeps, so we end up failing because the thing we're trying to install
is not already installed. Check before each point of use instead.

jhbuild/modtypes/meson.py
@@ -52,9 +52,8 @@ class MesonModule(MakeModule, DownloadableModule):
         self.skip_install_phase = skip_install_phase
         self.force_non_srcdir_builds = True
         self.supports_install_destdir = True
-        self.find_ninja()
 
-    def find_ninja(self):
+    def ensure_ninja_binary(self):
         for f in ['ninja', 'ninja-build']:
             if inpath(f, os.environ['PATH'].split(os.pathsep)):
                 self.ninja_binary = f
@@ -131,6 +130,7 @@ class MesonModule(MakeModule, DownloadableModule):
     def do_clean(self, buildscript):
         buildscript.set_action(_('Cleaning'), self)
         builddir = self.get_builddir(buildscript)
+        self.ensure_ninja_binary()
         buildscript.execute(self.ninja_binary + ' clean', cwd=builddir, extra_env=self.extra_env)
     do_clean.depends = [PHASE_CONFIGURE]
     do_clean.error_phases = [PHASE_FORCE_CHECKOUT, PHASE_CONFIGURE]
@@ -138,6 +138,7 @@ class MesonModule(MakeModule, DownloadableModule):
     def do_build(self, buildscript):
         buildscript.set_action(_('Building'), self)
         builddir = self.get_builddir(buildscript)
+        self.ensure_ninja_binary()
         buildscript.execute(self.ninja_binary, cwd=builddir, extra_env=self.extra_env)
     do_build.depends = [PHASE_CONFIGURE]
     do_build.error_phases = [PHASE_FORCE_CHECKOUT, PHASE_CONFIGURE]
@@ -151,6 +152,7 @@ class MesonModule(MakeModule, DownloadableModule):
         destdir = self.prepare_installroot(buildscript)
         extra_env = (self.extra_env or {}).copy()
         extra_env['DESTDIR'] = destdir
+        self.ensure_ninja_binary()
         buildscript.execute(self.ninja_binary + ' install', cwd=builddir, extra_env=extra_env)
         self.process_install(buildscript, self.get_revision())
     do_install.depends = [PHASE_BUILD]