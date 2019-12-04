82814cdd8aa10b0f25d642a379592941a5afb7ab
Christoph Reiter <reiter.christoph@gmail.com>
2019-12-04 09:18:54 +0100

    CI: Use Ubuntu 19.10
    19.04 will be EOL shortly

--- .gitlab-ci/build-docker.sh
+++ .gitlab-ci/build-docker.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/jhbuild/jhbuild:v3"
+TAG="registry.gitlab.gnome.org/gnome/jhbuild/jhbuild:v4"
 
 sudo docker build --tag "${TAG}" --file "Dockerfile" .
