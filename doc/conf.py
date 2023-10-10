5fa7b71e5bc1b41d1f99a54fc3ad6653977c7a15
Patrick Griffis <pgriffis@igalia.com>
2023-10-10 17:34:34 -0500

    docs: Switch to rtd theme

--- doc/conf.py
+++ doc/conf.py
@@ -2,15 +2,27 @@ project = 'JHBuild'
 copyright = '2023, JHBuild Contributors'
 author = 'James Henstridge'
 
+extensions = [
+    'sphinx_rtd_theme',
+]
 
-extensions = []
+html_theme = 'sphinx_rtd_theme'
+html_baseurl = 'https://gnome.pages.gitlab.gnome.org/jhbuild/'
+html_static_path = ['_static']
+html_context = {
+    'display_gitlab': True,
+    'gitlab_host': 'gitlab.gnome.org',
+    'gitlab_user': 'GNOME',
+    'gitlab_repo': 'jhbuild',
+    'gitlab_version': 'master',
+    'conf_py_path': '/doc/',
+    
+}
+html_theme_options = {
+}
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
 language = 'en'
 locale_dirs = ['po']
 gettext_compact = 'docs'
