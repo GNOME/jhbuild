af618722e2ab1e559f1500f6504c2deead397efb
Patrick Griffis <pgriffis@igalia.com>
2024-10-11 10:42:50 -0500

    CI: Add coverage reports

--- .gitlab-ci/build-docker.sh
+++ .gitlab-ci/build-docker.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/jhbuild/jhbuild:v5"
+TAG="registry.gitlab.gnome.org/gnome/jhbuild/jhbuild:v6"
 
 sudo docker build --tag "${TAG}" --file "Dockerfile" .
